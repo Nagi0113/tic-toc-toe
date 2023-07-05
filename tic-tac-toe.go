8532c20529c53ddec7316947b97b2a07238b8a07
Takuto Hashibe <m235586@hiroshima-u.ac.jp>
2023-07-05 02:38:39 +0000

    add input function

--- tic-tac-toe.go
+++ tic-tac-toe.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//  "reflect"
+	"strconv"
+)
 
 func main() {
-	var zahyo string
+	// 	for i := 0; ; i++{
+	//      if i%2 == 0 {
+	//         input()[0]
+	//      }
+	//      else{
+	//          input()[1]
+	//      }
+	// 	}
+	var zahyoint [2]int = input()
+	_ = zahyoint
+	fmt.Println(zahyoint[0])
+	fmt.Println(zahyoint[1])
 
+}
+
+func input() [2]int {
+	var zahyostr string
 	fmt.Println("input: ")
-	fmt.Scanln(&zahyo)
-
- 	fmt.Println("your input :" + zahyo)
+	fmt.Scanln(&zahyostr)
+	x, _ := strconv.Atoi(zahyostr[0:1])
+	y, _ := strconv.Atoi(zahyostr[2:3])
+	var zahyoint [2]int = [2]int{x, y}
+	return zahyoint
 }
